643ca2d90736f289fb7c608bf5988d0085d7f17d
Hritick Jaiswal <newbiehritick@gmail.com>
2022-01-19 10:15:40 +0530

    Added first look next add button and hookup firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { Fragment } from "react";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>Evernote Clone</title>
+        <meta
+          name="description"
+          content="An app for notes, so that nothing important slips from your mind"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import Image from "next/image";
+
 import styles from "../styles/Home.module.scss";
 
+import logo from "../public/evernote-icon.svg";
+
 export default function Home() {
-  return <div className={styles.container}>Hi</div>;
+  return (
+    <main className={styles.main}>
+      <article className={styles.container}>
+        <div className={styles.container__imageBox}>
+          <Image
+            src={logo}
+            objectFit="fill"
+            className={styles.container__image}
+          ></Image>
+        </div>
+        <h1 className={styles.container__title}>Evernote</h1>
+        <h2 className={styles.container__tagline}>
+          Remember everything important.
+        </h2>
+      </article>
+    </main>
+  );
 }
